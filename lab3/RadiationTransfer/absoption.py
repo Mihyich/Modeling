b72178681a2494f471e0c3e613d9a8bc89525c35
GrizlyBear <michail.zevahin@gmail.com>
2025-05-18 13:45:35 +0300

    Завершение программы при выбрасывании исключения

--- lab3/RadiationTransfer/absoption.py
+++ lab3/RadiationTransfer/absoption.py
@@ -16,10 +16,13 @@ def initAbsorptionCoefTable(filePath: Path, variant: int) -> tuple[list[float], 
             k = [float(t[1]) for t in table]
     except FileNotFoundError:
         print(f"Ошибка: Файл '{filePath}' не найден.")
+        exit(1)
     except IOError:
         print(f"Ошибка: Проблема с чтением файла '{filePath}'.")
+        exit(1)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
+        exit(1)
     finally:
         return (T, k)
 
