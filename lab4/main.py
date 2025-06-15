a5923e4993cc2c70c4a9c933a7e0f152b7f64b3f
GrizlyBear <michail.zevahin@gmail.com>
2025-06-15 18:52:15 +0300

    Отображение графика

--- lab4/main.py
+++ lab4/main.py
@@ -1,27 +1,17 @@
-from Core.defines import filePathODE, filePathPDE, variant, N, R, T0, Tw, p, tau, I_max, t_max, S
+from Core.defines import filePathODE, filePathPDE, folderPathPlot, variant, N, R, T0, Tw, p, tau, I_max, t_max, S
+from Core.plotter import Plotter
 from ODE.absorption import initAbsorptionCoefTable as initAbsorpCoefODE
 from PDE.absorption import initAbsorptionCoefTable as initAbsorpCoefPDE
 from PDE.solution import solutePDE
 
-import matplotlib.pyplot as plt
-
 
 def main():
+    plotter = Plotter(folderPathPlot)
     T_tableODE, K_table = initAbsorpCoefODE(filePathODE, variant)
     T_tablePDE, sigma_table, lambda_table, cT_table = initAbsorpCoefPDE(filePathPDE)
+    r, Thistory = solutePDE(N, R, T0, Tw, p, tau, I_max, t_max, S, T_tableODE, K_table, T_tablePDE, sigma_table, lambda_table, cT_table, plotter)
 
-    r, Thistory = solutePDE(N, R, T0, Tw, p, tau, I_max, t_max, S, T_tableODE, K_table, T_tablePDE, sigma_table, lambda_table, cT_table)
-
-    plt.figure(figsize=(10, 6))
-    for i, T in enumerate(Thistory):
-        if i % 10 == 0 or i == len(Thistory) - 1:
-            plt.plot(r, T, label=f"t = {i * tau:.2e} с")
-    plt.xlabel("r, см")
-    plt.ylabel("T(r), К")
-    plt.title("Температурное поле во времени")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    plotter.plot_T_evolution(r, Thistory, tau)
 
 
 if __name__ == "__main__":
